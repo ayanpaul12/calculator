b67278717959e231b7ce2c5d9ee6e4a29b3a8527
ayanpaul12 <104326463+ayanpaul12@users.noreply.github.com>
2022-07-16 00:57:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+function clearScreen() {
+    document.querySelectorAll(".result")[0].value = "";
+}
+ 
+// This function display values
+function display(value) {
+    document.querySelectorAll(".result")[0].value += value;
+}
+ 
+// This function evaluates the expression and returns result
+function calculate() {
+    var p = document.querySelectorAll(".result")[0].value;
+    var q = eval(p);
+    document.querySelectorAll(".result")[0].value = q;
+}
