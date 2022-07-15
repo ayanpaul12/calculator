d2deffb2fe422b71343abbf70942b4d4f7a9856f
ayanpaul12 <104326463+ayanpaul12@users.noreply.github.com>
2022-07-16 00:51:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,5 @@ function display(value) {
 function calculate() {
     var p = document.querySelectorAll(".result")[0].value;
     var q = eval(p);
-    alert(q);
     document.querySelectorAll(".result")[0].value = q;
 }
